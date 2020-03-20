8fd4d0508b531691571bfa9433e78a7ead532e38
Paul Cortes <paulkerg@gmail.com>
2020-03-19 23:41:44 -0500

    add Model School and delete obj directory

---
+++ Models/School.cs
@@ -0,0 +1,10 @@
+namespace platzi_asp_net_core.Models
+{
+    public class School
+    {
+        public string SchoolId { get; set; }
+        public string Name { get; set; }
+        public int FoundationYear { get; set; }
+
+    }
+}
