130398accdfa818c43d8ddaa6fb54bd953be1eba
Paul Cortes <paulkerg@gmail.com>
2020-03-19 23:47:59 -0500

    create model school in SchoolController@Index send model to view, and delete bin directory

--- Controllers/SchoolController.cs
+++ Controllers/SchoolController.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using platzi_asp_net_core.Models;
 
 namespace platzi_asp_net_core.Controllers
 {
@@ -6,7 +8,11 @@ namespace platzi_asp_net_core.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var school = new School();
+            school.FoundationYear = 2005;
+            school.SchoolId = Guid.NewGuid().ToString();
+            school.Name = "Platzi School";
+            return View(school);
         }
     }
 }
