8097fa152c52cfc230a88349926f5464d2d853b8
Paul Cortes <paulkerg@gmail.com>
2020-03-25 22:53:31 -0500

    Modify View ObjectSimpleSubjet and add Method and View MultiSubject

--- Controllers/SubjectController.cs
+++ Controllers/SubjectController.cs
@@ -1,19 +1,32 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using platzi_asp_net_core.Models;
 
 namespace platzi_asp_net_core.Controllers
 {
-    public class SubjectController : Controller
+  public class SubjectController : Controller
+  {
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            var subject = new Subject();
-            subject.Name = "Programming";
-            ViewBag.ThingDinamic = "The nun";
-            ViewBag.date = DateTime.Now;
-
-            return View(subject);
-        }
+        return View();
     }
+    public IActionResult MultiSubject()
+    {
+      var subject = new Subject();
+      subject.Name = "Programming";
+      ViewBag.ThingDinamic = "The nun";
+      ViewBag.date = DateTime.Now;
+
+      var subjectList = new List<Subject>() {
+        new Subject {Name = "Matemáticas"},
+        new Subject {Name = "Educación Física"},
+        new Subject {Name = "Castellano"},
+        new Subject {Name = "Ciencias Naturales"},
+        new Subject {Name = "Programacion"}
+      };
+
+      return View("MultiSubject",subjectList);
+    }
+  }
 }
